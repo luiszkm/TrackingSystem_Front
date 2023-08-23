82c3a0a0238d7cff81d6ec8e9ac686f6519954ba
luis <luiszkm@gmail.com>
2023-08-23 09:32:55 -0300

    feat: add admin page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { useEffect, useRef } from 'react'
+import { useMap } from '../hooks/useMap'
+import { Route } from '../utils/model'
+import { socket } from '../utils/socket-io'
+
+export default function AdminPage() {
+  const mapContainerRef = useRef<HTMLDivElement>(null)
+  const map = useMap(mapContainerRef)
+
+  useEffect(() => {
+    socket.connect()
+    socket.on(
+      'new-point',
+      async (data: { route_id: string; lat: number; lng: number }) => {
+        const hasRoute = map?.hasRoute(data.route_id)
+        if (!hasRoute) {
+          const response = await fetch(
+            `http://localhost:3000/routes/${data.route_id}`
+          )
+          const route: Route = await response.json()
+          map?.removeRoute(data.route_id)
+          await map?.addRouteWithIcons({
+            routeId: data.route_id,
+            startMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location
+            },
+            endMarkerOptions: {
+              position: route.directions.routes[0].legs[0].end_location
+            },
+            carMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location
+            }
+          })
+        }
+        map?.moveCar(data.route_id, { lat: data.lat, lng: data.lng })
+      }
+    )
+    return () => {
+      socket.disconnect()
+    }
+  }, [map])
+
+  return (
+    <main className=" h-full w-full ">
+      <div id="map" className="h-full w-full" ref={mapContainerRef}></div>
+    </main>
+  )
+}
--- src/app/driver/page.tsx
+++ src/app/driver/page.tsx
@@ -1,17 +1,17 @@
 'use client'
-import { useRef,  } from 'react'
+import { useEffect, useRef } from 'react'
 import { useMap } from '../hooks/useMap'
 
 import useSwr from 'swr'
 import { fetcher } from '../utils/http'
 import { Route } from '../utils/model'
+import { socket } from '../utils/socket-io'
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 export default function DriverPAge() {
   const mapContainerRef = useRef<HTMLDivElement>(null)
   const map = useMap(mapContainerRef)
-
 
   const {
     data: routes,
@@ -21,36 +21,51 @@ export default function DriverPAge() {
     fallback: []
   })
 
- 
   async function handleStartRoute() {
     const routeId = document.querySelector<HTMLSelectElement>('#route')!.value
     const response = await fetch(`http://localhost:3000/routes/${routeId}`)
     const route = await response.json()
 
-
     map?.removeAllRoutes()
     map?.addRouteWithIcons({
       routeId: routeId,
       startMarkerOptions: {
-        position: route.directions.routes[0].legs[0].start_location,
+        position: route.directions.routes[0].legs[0].start_location
       },
       endMarkerOptions: {
-        position: route.directions.routes[0].legs[0].end_location,
+        position: route.directions.routes[0].legs[0].end_location
       },
       carMarkerOptions: {
-        position: route.directions.routes[0].legs[0].start_location,
-      },
+        position: route.directions.routes[0].legs[0].start_location
+      }
     })
 
-    const {steps} = route.directions.routes[0].legs[0]
+    const { steps } = route.directions.routes[0].legs[0]
     for (const step of steps) {
-      await sleep(2000);
+      await sleep(2000)
       map?.moveCar(routeId, step.start_location)
+      socket.emit('new-point',{
+        route_id: routeId,
+        lat: step.start_location.lat,
+        lng: step.start_location.lng
+      })
 
-      await sleep(2000);
-      map?.moveCar(routeId, step.end_location);
+      await sleep(2000)
+      map?.moveCar(routeId, step.end_location)
+      socket.emit('new-point',{
+        route_id: routeId,
+        lat: step.end_location.lat,
+        lng: step.end_location.lng
+      })
     }
   }
+
+  useEffect(() => {
+    socket.connect()
+    return () => {
+      socket.disconnect()
+    }
+  },[])
 
   return (
     <main className="flex h-full ">
@@ -66,7 +81,9 @@ export default function DriverPAge() {
             ))}
           </select>
 
-          <button type="submit" onClick={handleStartRoute}>iniciar a viagem</button>
+          <button type="submit" onClick={handleStartRoute}>
+            iniciar a viagem
+          </button>
         </div>
       </div>
       <div id="map" className="h-full w-full" ref={mapContainerRef}></div>
